3c5b493609cb31d0d02d50f984011c3230ec1d0d
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-04-08 09:39:52 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+      var a = 1;          
+            function add(){
+
+                document.getElementById("amount").innerHTML=a++;
+                
+            }
+            
+            function remove(){
+                document.getElementById("amount").innerHTML=a--;
+            }
