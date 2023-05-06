ea6c57f0621b68755ca94b030de396fdd21c8943
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-05-07 07:28:18 +1200

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,13 @@
             function remove(){
                 document.getElementById("amount").innerHTML=a--;
             }
+            
+            function multiply(){
+                document.getElementById("amount").innerHTML=a * a;
+            }
+
+            if(document.getElementById("amount").value < 0){
+                  document.getElementById("hi").innerHTML="Emotional Damage has occured please increase brain cells";}
+
+            id(document.getElementById("amount").value > 100){
+                  document.getElementById("amount").innerHTML="you have become smart but this is just 10% of how smart you can become";
